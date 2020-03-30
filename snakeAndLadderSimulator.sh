b54565fc259b1143abe1b31f9315e828a69573fc
pranav8596 <pige786123@gmail.com>
2020-03-30 18:18:13 +0530

    ADDED: Checked for the options such as NoPlay,Ladder and Snake

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -2,9 +2,32 @@
 
 #Constants
 INITIAL_POSITION=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
+#Variables
+playerPosition=0
 
 #To roll a die
 function rollTheDie() {
 	dieNumber=$((RANDOM%6+1))
 }
-rollTheDie
+
+#To check for No Play,Ladder or Snake options
+function checkForOptions() {
+	rollTheDie
+	checkOptions=$((RANDOM%3))
+	case $checkOptions in
+		$NO_PLAY)
+			playerPosition=$playerPosition
+			;;
+		$LADDER)
+			playerPosition=$(($playerPosition + $dieNumber))
+			;;
+		$SNAKE)
+			playerPosition=$(($playerPosition - $dieNumber))
+			;;
+	esac
+}
+checkForOptions
